f6743b0d8509b2becfad3b84d12bf3b541e7f80b
mayankrajput2682-beep <mayankrajput2682@gmail.com>
2025-12-31 15:03:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const checkboxes = document.querySelectorAll("input[type='checkbox']");
+const rows = document.querySelectorAll(".row");
 
-checkboxes.forEach((checkbox, index) => {
-    checkbox.checked = localStorage.getItem("task" + index) === "true";
+rows.forEach(row => {
+  const subject = row.dataset.subject;
+  const targetsDiv = row.querySelector(".targets");
+  const addBtn = row.querySelector(".addBtn");
 
-    checkbox.addEventListener("change", () => {
-        localStorage.setItem("task" + index, checkbox.checked);
+  let tasks = JSON.parse(localStorage.getItem(subject)) || [];
+
+  function save() {
+    localStorage.setItem(subject, JSON.stringify(tasks));
+  }
+
+  function render() {
+    targetsDiv.innerHTML = "";
+    tasks.forEach((task, index) => {
+      const div = document.createElement("div");
+      div.className = "task";
+      div.innerHTML = `
+        <label>
+          <input type="checkbox" ${task.done ? "checked" : ""}>
+          ${task.name}
+        </label>
+        <button>X</button>
+      `;
+
+      div.querySelector("input").addEventListener("change", e => {
+        tasks[index].done = e.target.checked;
+        save();
+      });
+
+      div.querySelector("button").addEventListener("click", () => {
+        tasks.splice(index, 1);
+        save();
+        render();
+      });
+
+      targetsDiv.appendChild(div);
     });
+  }
+
+  addBtn.addEventListener("click", () => {
+    const name = prompt("Enter Target (PYQ, Module, Chapter etc.)");
+    if (name) {
+      tasks.push({ name, done:false });
+      save();
+      render();
+    }
+  });
+
+  render();
 });
